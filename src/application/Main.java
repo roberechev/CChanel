49b66bfc753e5ab9dc13b0d0f4167206f38aa0d2
roberechev <rober.eche.sa@gmail.com>
2019-12-04 18:47:22 +0100

    documentacion

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,5 @@
 package application;
-	
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
@@ -7,19 +7,29 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
-
+/**
+ * 
+ * @author rober
+ * @see opciones
+ * @version 1.0
+ * 
+ *          El programa consiste en un fondo con dos botones, uno cierra todo el
+ *          programa el otro abre otra ventana con un boton para cerrar esa
+ *          ventana.
+ *
+ */
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {		
+	public void start(Stage primaryStage) {
 		try {
-			FXMLLoader loader=new FXMLLoader(getClass().getResource("vista.fxml"));
-			//BorderPane root = new BorderPane();
-			AnchorPane root =(AnchorPane)loader.load();
-			Scene scene = new Scene(root,570,320);
-			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("vista.fxml"));
+			// BorderPane root = new BorderPane();
+			AnchorPane root = (AnchorPane) loader.load();
+			Scene scene = new Scene(root, 570, 320);
+			// scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
